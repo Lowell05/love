f1028bbc447ed371723fa45764a24bbbe5dd38c5
Lowell05 <39002307+Lowell05@users.noreply.github.com>
2019-01-09 20:48:50 +0800

    first commit

---
+++ lowell/js/member.js
@@ -0,0 +1,490 @@
+var member = [
+  {
+    "phone": "0001",
+    "name": "img/1.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/2.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/3.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/4.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/5.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/6.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/7.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/8.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/9.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/10.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/11.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/12.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/13.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/14.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/15.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/16.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/17.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/18.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/19.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/20.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/21.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/22.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/23.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/24.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/25.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/26.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/27.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/28.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/29.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/30.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/31.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/32.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/33.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/34.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/35.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/36.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/37.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/38.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/39.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/40.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/41.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/42.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/43.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/44.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/45.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/46.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/47.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/48.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/49.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/5.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/51.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/52.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/53.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/54.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/55.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/56.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/57.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/58.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/59.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/60.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/61.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/62.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/63.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/64.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/65.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/66.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/67.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/68.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/69.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/70.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/71.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/72.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/73.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/74.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/75.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/76.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/77.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/78.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/79.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/80.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/81.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/82.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/83.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/84.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/85.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/86.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/87.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/88.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/89.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/90.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/91.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/92.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/93.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/94.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/95.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/96.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/97.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/98.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/99.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/100.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/101.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/102.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/103.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/104.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/105.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/106.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/107.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/108.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/109.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/110.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/111.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/112.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/113.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/114.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/115.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/116.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/117.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/118.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/119.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/120.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/121.jpg"
+  },
+  {
+    "phone": "0001",
+    "name": "img/122.jpg"
+  }
+];
